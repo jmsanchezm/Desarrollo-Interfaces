9bd6ed5f0e1701dbd6f459fdca20eddb92481ad6
jmsanchezm <juanm.sanchez@iesnervion.es>
2024-02-04 22:50:18 +0100

    Quedan los viewModels de las manejadoras y las views

--- Tema11/CRUD/Capa BL/obj/Debug/net7.0-maccatalyst/Capa BL.AssemblyInfo.cs
+++ Tema11/CRUD/Capa BL/obj/Debug/net7.0-maccatalyst/Capa BL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Capa BL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d843d988a159e05ce347e8daafe122745a145d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Capa BL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Capa BL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
